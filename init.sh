d35ce71544356c950e20a51fdea71e5654f97476
xuke <lovecpp@gmail.com>
2023-06-30 22:26:50 -0400

    build init

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 [ -e /usr/local/adk/cmdline-tools ] || cp -rf /usr/local/sdk/cmdline-tools /usr/local/adk/
-cd /usr/local/sdk/
+cd /usr/local/adk/
 MAN=./cmdline-tools/latest/bin/sdkmanager
 yes | $MAN --licenses
 ls
